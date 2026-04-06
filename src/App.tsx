5ab3438d95f80bc685ab8a43d04645de55f40f68
showzib <showzibtanveer11@gmail.com>
2026-04-06 13:24:04 +0500

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+    <div className="bg-secondary-500 text-primary-500 p-4">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        text: "var(--color-text)",
+        background: "var(--color-bg)",
+      },
+    },
+  },
+  plugins: [],
+}
